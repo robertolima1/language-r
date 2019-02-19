a5b0966a0d9b29e4448b92e2dddaa775fe43ee65
roberto <roberto@gmail.com>
2019-02-19 20:09:23 -0300

    Incrementando exemplos com plotagem de dados

--- PrimeiroExemplo.R
+++ PrimeiroExemplo.R
@@ -3,3 +3,13 @@ Carbono <- c(8, 54, 534, 1630, 6611)
 combustivelfossil<-data.frame(Ano, Carbono)
 plot(Ano ~ Carbono, data=combustivelfossil, pch=16)
 range(combustivelfossil)               
+
+
+plot(iris$Petal.Length, iris$Petal.Width, main="Edgar Anderson's Iris Data")
+
+x <- read_xls("/home/ime/Downloads/paraboloide.xls", sheet = NULL, range = NULL, col_names = TRUE,
+              col_types = NULL, na = "", trim_ws = TRUE, skip = 0,
+              guess_max = min(1000, 1000),
+              progress = readxl_progress(), .name_repair = "unique")
+
+plot(x$fXY, x$fYX, main="Dados de paraboloide")
